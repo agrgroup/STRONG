f6dd93c63d50fe3f5f421629d97d3e1d0abd8711
AGR Group @ IISc Chemical Engineering <70911775+agrgroup@users.noreply.github.com>
2024-02-20 19:17:22 +0530

    Add files via upload

---
+++ Comparing_Nanopores/matlab_files/main.m
@@ -0,0 +1,21 @@
+clc
+close all
+clear all
+
+%% adding matlab_files folder on path of matlab
+cd ..
+addpath('matlab_files')
+%% generating strongs
+tic
+generate_strongs()
+disp('STRONGS have been generated in ')
+toc 
+%% implementing symmetry operations to obtain unique xyz files
+tic
+unique_strongs()
+disp('unique files generated')
+toc
+
+%%
+
+
