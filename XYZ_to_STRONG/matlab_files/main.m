67d654030017ed834c13f8f97e563938ae9346d4
AGR Group @ IISc Chemical Engineering <70911775+agrgroup@users.noreply.github.com>
2024-02-29 09:38:03 +0530

    Update main.m

--- XYZ_to_STRONG/matlab_files/main.m
+++ XYZ_to_STRONG/matlab_files/main.m
@@ -8,19 +8,8 @@ addpath('matlab_files')
 %% generating strongs
 tic
 generate_strongs()
-disp('STRONGS have been generated in ')
+disp('STRONGS have been generated in')
 toc 
-%% implementing symmetry operations to obtain unique xyz files
-%tic
-%unique_strongs()
-%disp('unique files generated')
-%toc
 
-%% implementing symmetry operation to obtain rotational and reflectional file combinations
-% tic
-% symmetric_cases()
-% disp('symmetric cases completed')
-% toc
-%%
 
 
